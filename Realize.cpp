f3d61f0a73c68393c1ced7eb3cd65120b0610df9
Konstanti B.V <43479284+Kostolom9919@users.noreply.github.com>
2018-12-01 02:00:57 +0300

    Add files via upload

--- Realize.cpp
+++ Realize.cpp
@@ -4,30 +4,30 @@ drob operator+ (drob a, drob b) {
 	drob s;
 	s.zn = Nok(a.zn, b.zn);
 	s.ch = a.ch*s.zn / a.zn + b.ch*s.zn / b.zn;
-	s.ch /= Nod(s.zn, fabs(s.ch));
-	s.zn /= Nod(s.zn, fabs(s.ch));
+	s.ch /= Nod(s.zn, abs(s.ch));
+	s.zn /= Nod(s.zn, abs(s.ch));
 	return s;
 }
 drob operator- (drob a, drob b) {
 	drob s;
 	s.zn = Nok(a.zn, b.zn);
 	s.ch = a.ch*s.zn / a.zn - b.ch*s.zn / b.zn;
-	s.ch /= Nod(s.zn, fabs(s.ch));
-	s.zn /= Nod(s.zn, fabs(s.ch));
+	s.ch /= Nod(s.zn, (int)(abs((int)s.ch)));
+	s.zn /= Nod(s.zn, (int)(abs((int)s.ch)));
 	return s;
 }
 drob operator* (drob a, drob b) {
 	a.ch *= b.ch;
 	a.zn *= b.zn;
-	a.ch /= Nod(fabs(a.ch), a.zn);
-	a.zn /= Nod(fabs(a.ch), a.zn);
+	a.ch /= Nod((int)(abs((int)a.ch)), a.zn);
+	a.zn /= Nod((int)(abs((int)a.ch)), a.zn);
 	return a;
 }
 drob operator/ (drob a, drob b) {
 	a.ch *= b.zn;
 	a.zn *= b.ch;
-	a.ch /= Nod(fabs(a.ch), a.zn);
-	a.zn /= Nod(fabs(a.ch), a.zn);
+	a.ch /= Nod((int)(abs((int)a.ch)), a.zn);
+	a.zn /= Nod((int)(abs((int)a.ch)), a.zn);
 	return a;
 }
 bool operator==(drob a, drob b) {
